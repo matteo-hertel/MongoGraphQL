f5614ee36661efe02ba0928399066bb65344891a
Matteo Hertel <info@matteohertel.com>
2018-06-17 22:31:57 +0100

    Add schema and resolvers

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log("Process started 🎉");
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./graphql/schema");
+const PORT = 3456;
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema
+  })
+);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ src/graphql/schema.js
@@ -0,0 +1,11 @@
+const { makeExecutableSchema } = require("graphql-tools");
+const resolvers = require("./resolvers");
+const typeDefs = `
+type Query{
+hello:String!
+}
+`;
+module.exports = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
